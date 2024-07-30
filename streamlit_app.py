e15e7a0e050fa8fe8534c5f6254fd0e6f10a915f
Etobeliac <celestin.clbt@gmail.com>
2024-07-30 12:33:38 +0000

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,14 @@
 import streamlit as st
+import os
 
-# Dictionnaire contenant les scripts disponibles et leur contenu
-scripts = {
-    "domain_classifier_app.py": """
-import streamlit as st
-
-def main():
-    st.title("Classification de noms de domaine")
-
-    st.file_uploader("Déposez votre fichier Excel ici", type=["xlsx"])
-
-    st.button("Classifier les domaines")
-
-    st.success("Ceci est un message de succès")
-
-if __name__ == "__main__":
-    main()
-    """,
-    "autre_script.py": "# Contenu de l'autre script ici",
-    "encore_un_script.py": "# Contenu du troisième script ici"
-}
+def read_script_content(script_name):
+    try:
+        with open(script_name, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "# Le fichier n'a pas été trouvé."
+    except Exception as e:
+        return f"# Une erreur s'est produite lors de la lecture du fichier : {str(e)}"
 
 def main():
     st.set_page_config(layout="wide")
@@ -27,16 +16,20 @@ def main():
     # Création de deux colonnes
     col1, col2 = st.columns([1, 3])
 
+    # Obtenir la liste des fichiers Python dans le répertoire courant
+    script_files = [f for f in os.listdir('.') if f.endswith('.py')]
+
     # Colonne de gauche pour la sélection du script
     with col1:
         st.header("Sélectionnez un script")
-        selected_script = st.selectbox("", list(scripts.keys()))
+        selected_script = st.selectbox("", script_files)
 
     # Colonne de droite pour afficher le contenu du script
     with col2:
         st.header("Page d'Accueil des Scripts")
         st.subheader(f"Contenu de {selected_script}")
-        st.code(scripts[selected_script], language='python')
+        script_content = read_script_content(selected_script)
+        st.code(script_content, language='python')
 
         # Bouton pour exécuter le script (simulation)
         if st.button("Exécuter le script"):
